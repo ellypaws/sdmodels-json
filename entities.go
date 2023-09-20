2eb21fbcd0abd71b8929b73b3c9409d5dd6a49ec
Elly <dev@keiau.space>
2023-09-20 19:47:38 +0800

    Improve file reading and sorting operations
    In this commit, the logic for reading files and parsing section data was improved and extended to support different model types (Lora, Checkpoint, Vae). The new implementation uses a series of regular expressions to sort and classify each line in the file according to its corresponding section. This provides a more flexible and robust approach for handling various file structures and formats. Additionally, we have implemented printing functionality for each type of model and the printEach function has been updated to handle various type of models and print them uniformly using a new Printables interface. Finally, error handling was improved within file handling functions for better reliability and simple debugging.

--- entities.go
+++ entities.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type Models struct {
 	Loras       []*Lora       `json:"loras,omitempty"`
 	Checkpoints []*Checkpoint `json:"checkpoints,omitempty"`
 	Vaes        []*Vae        `json:"vaes,omitempty"`
 	Embeddings  []*Embedding  `json:"embeddings,omitempty"`
+}
+
+type Printables interface {
+	SPrint() string
 }
 
 type Lora struct {
@@ -13,10 +19,18 @@ type Lora struct {
 	Extension string `json:"extension"`
 }
 
+func (l *Lora) SPrint() string {
+	return fmt.Sprintf("\n[Lora]\nFolder: %s\nFilename: %s\nExtension: %s\n", l.Folder, l.Filename, l.Extension)
+}
+
 type Checkpoint struct {
 	Folder    string `json:"folder,omitempty"`
 	Filename  string `json:"filename"`
 	Extension string `json:"extension"`
+}
+
+func (c Checkpoint) SPrint() string {
+	return fmt.Sprintf("\n[Checkpoint]\nFolder: %s\nFilename: %s\nExtension: %s\n", c.Folder, c.Filename, c.Extension)
 }
 
 type Vae struct {
@@ -25,8 +39,16 @@ type Vae struct {
 	Extension string `json:"extension"`
 }
 
+func (v Vae) SPrint() string {
+	return fmt.Sprintf("\n[Vae]\nFolder: %s\nFilename: %s\nExtension: %s\n", v.Folder, v.Filename, v.Extension)
+}
+
 type Embedding struct {
 	Folder    string `json:"folder,omitempty"`
 	Filename  string `json:"filename"`
 	Extension string `json:"extension"`
+}
+
+func (e Embedding) SPrint() string {
+	return fmt.Sprintf("\n[Embedding]\nFolder: %s\nFilename: %s\nExtension: %s\n", e.Folder, e.Filename, e.Extension)
 }
--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 
 func main() {
 	var filename string
-	currentDirectory := os.Getenv("PWD")
-	fmt.Printf("Current directory: %s\n", currentDirectory)
-	fmt.Print("Enter the filename: ")
 	// if loras.txt exists in current directory, use that
 	if _, err := os.Stat("loras.txt"); err == nil {
 		filename = "loras.txt"
 	} else {
+		fmt.Print("Enter the filename: ")
 		_, _ = fmt.Scan(&filename)
 	}
 	models := Create()
-	models.CreateLora()
-	models.ReadLoraFromFile(filename)
+	//models.CreateLora()
+	//models.ReadLoraFromFile(filename)
+	//models.printEach()
+	models.ReadFromFileAndSort(filename)
 	models.printEach()
-	//StringToLora(loraSlice)
 }
